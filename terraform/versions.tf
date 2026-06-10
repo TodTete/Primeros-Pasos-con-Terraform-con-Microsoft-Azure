6ccf1641d4f1ca9dc66e7af2af684d67a7273816
Ricardo Vallejo Sánchez <131738372+TodTete@users.noreply.github.com>
2026-06-09 20:46:49 -0600

    Define required Terraform version and providers
    Set minimum Terraform version and specify Azure provider.

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,13 @@
+# Define la versión mínima de Terraform requerida para este proyecto.
+terraform {
+  required_version = ">= 1.6.0"
 
+  # Especifica el proveedor que se utilizará para administrar recursos en Azure.
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      # Se recomienda utilizar una versión compatible y estable.
+      version = "~> 4.0"
+    }
+  }
+}
